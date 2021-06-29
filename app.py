959b9f8c6f33541a93a52e0d270108e91db66715
Morteza Jamali <mortezajamali4241@gmail.com>
2021-06-29 12:52:20 +0430

    feat: GitPython package added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 import yaml
+import shutil
+from tempfile import TemporaryDirectory
+from git import Repo
 
 CONFIG_FILE = 'monosplit.yml'
 
@@ -11,27 +14,44 @@ def init():
         print('Directory already exist !')
 
 
-def getConfig():
+def get_config():
     data = yaml.load(open(CONFIG_FILE), Loader=yaml.FullLoader)
 
     print(data)
 
 
-def scanProject(path=".", configs=[]):
-    if hasConfig(path):
+def scan_project(path=".", configs=[]):
+    if has_config(path):
         configs.append(os.path.join(path, CONFIG_FILE))
 
     entries = os.scandir(path)
 
     for entry in entries:
         if entry.is_dir():
-            scanProject(entry, configs)
+            scan_project(entry, configs)
 
     return configs
 
 
-def hasConfig(path="."):
+def has_config(path="."):
     return os.path.exists(os.path.join(path, CONFIG_FILE))
 
 
-print(scanProject())
+def push_repository(repo_url, repo_path):
+    with TemporaryDirectory() as tmpdir:
+        print('Temporary directory is', tmpdir)
+
+        cloned_repo = Repo.clone_from(repo_url, tmpdir)
+
+        print(cloned_repo.is_dirty())
+
+        shutil.copytree(repo_path, tmpdir, dirs_exist_ok=True)
+        entries = os.scandir(tmpdir)
+
+        for entry in entries:
+            print(entry)
+
+        print(cloned_repo.is_dirty())
+
+def get_latest_commits(path="."):
+    pass
